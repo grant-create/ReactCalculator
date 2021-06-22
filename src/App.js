80f1bbd7eac8fcb91f8826d2b58b3cbd61c1f989
grant-create <grant.s.wilkie@gmail.com>
2021-06-22 09:03:25 -0700

    Still have some bugs

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import Calculator from './Calculator'
+
+export default class App extends Component{
+  render() {
+    return(
+      <div>
+        <h1>yo</h1>
+        <Calculator/>
+      </div>
+    )
+  }
+}
